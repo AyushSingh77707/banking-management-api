2de71ffff6faf6f6ef7af0cafc23943a291409cd
ayush <ayush@gmail.com>
2026-09-09 10:34:59 +0530

    feat(customer):Add pagination schema and pagination in customer entity

--- app/crud/customer.py
+++ app/crud/customer.py
@@ -25,8 +25,9 @@ def get_customer(cust_id:int , db:Session):
         select(Customer).where(Customer.customer_id==cust_id)
     )
 
-def get_all_customers(db:Session):
-    return db.scalars(select(Customer)).all()
+def get_all_customers(db:Session,limit:int=10,offset:int=0):
+    query=select(Customer).limit(limit).offset(offset)
+    return db.scalars(query).all()
 
 def update_customer(cust_id:int,data:CustomerUpdate,db:Session):
     c=db.scalar(select(Customer).where(Customer.customer_id==cust_id))
--- app/routers/customer.py
+++ app/routers/customer.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter,Depends,HTTPException,status
+from fastapi import APIRouter,Depends,HTTPException,status,Query
 from sqlalchemy.orm import Session 
 from app.core.database import get_db
 from app.crud.customer import create_customer,get_customer,get_all_customers,update_customer,delete_customer
 from app.schemas.customer import CustomerCreate,CustomerResponse,CustomerUpdate
+from app.schemas.pagination import PaginationPara
 
 
 router=APIRouter(prefix="/customers",tags=["Customer"])
@@ -13,8 +14,8 @@ def customer_create_endpoint(data:CustomerCreate,db:Session=Depends(get_db)):
     return create_customer(customer=data,db=db)
 
 @router.get("/",response_model=list[CustomerResponse])
-def get_customers(db:Session=Depends(get_db)):
-    return get_all_customers(db)
+def get_customers(db:Session=Depends(get_db),pagination:PaginationPara=Depends()):
+    return get_all_customers(db,limit=pagination.limit,offset=pagination.offset)
 
 @router.get("/{id}",response_model=CustomerResponse)
 def get_customer_by_id(id:int,db:Session=Depends(get_db)):
